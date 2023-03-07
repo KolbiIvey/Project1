3d4ac8436da929ba35969e198fbe54c6e7edfaa7
KolbiIvey <kolbiivey@gmail.com>
2023-03-07 11:52:46 -0700

    update

--- game.js
+++ game.js
@@ -1,45 +1,55 @@
   /*----- constants -----*/
-const windowValues = [2, 8, 5]
+const WINDOW_VALUES = [2, 8, 5]
 
   /*----- state variables -----*/
 let scores;
 
 let balance;
 
+let bet;
 
 
   /*----- cached elements  -----*/
-
+const slotWindowEls = document.querySelectorAll('.slotMachine');
+const betBtnEl = document.querySelector('#betButton')
 
   /*----- event listeners -----*/
 document.querySelector('#spinButton').addEventListener('click', spin);
-document.querySelector('#betButton').addEventListener('click', bet)
-document.querySelector('#cashOutButton').addEventListener('click', cashingOut)
+// document.querySelector('#betButton').addEventListener('click', bet)
+// document.querySelector('#cashOutButton').addEventListener('click', cashingOut)
 
   /*----- functions -----*/
- init();
+  init();
 
 
-  function init() {
-     scores = { 
-        win: 0,
-        lost: 0
-     };
-     balance = 0
+   function init() {
+      scores = { 
+            win: 0,
+            lost: 0
+      };
+     balance = 500
+
+      bet = 0
      render();
   }
 
-  function spin (
+   function spin() {
 //Spin needs to make the spin button cyle through the windowValues array in each window
+     let randomNum = WINDOW_VALUES[Math.floor(Math.random() * WINDOW_VALUES.length)]
+      slotWindowEls.innerHTML = randomNum;
+      return randomNum
+  }
 
-  )
-
-  function bet() {
-//bet needs to ask the player how much they want to bet from
-//their available balance in incriments of 20$ up to 100$
+  function wagerAmount() {
+//bet needs to allow to player to bet 5$ or 10$ from their available balance
+    let newWager = 
   }
 
   function cashingOut() {
 //cashingOut need to essentially take the players current balance and cash it out
 //(end the game and reset)
+  }
+
+  function render() {
+    spin();
   }
