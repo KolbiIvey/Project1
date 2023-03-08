6cb81eb44a16e6691324996de18de3d70c02be3d
KolbiIvey <kolbiivey@gmail.com>
2023-03-08 11:16:04 -0700

    new update

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
   /*----- constants -----*/
-const WINDOW_VALUES = [2, 8, 5]
+const WINDOW_VALUES = [2, 8, 5,]
 
   /*----- state variables -----*/
 let scores;
@@ -10,8 +10,10 @@ let bet;
 
 
   /*----- cached elements  -----*/
-const slotWindowEls = document.querySelectorAll('.slotMachine');
+const slotWindowEls = Array.from(document.querySelectorAll('.slotMachine'));
 const betBtnEl = document.querySelector('#betButton')
+const scoresEl = document.querySelector('#scores')
+const balanceEl = document.querySelector('#balance')
 
   /*----- event listeners -----*/
 document.querySelector('#spinButton').addEventListener('click', spin);
@@ -35,15 +37,38 @@ document.querySelector('#spinButton').addEventListener('click', spin);
 
    function spin() {
 //Spin needs to make the spin button cyle through the windowValues array in each window
-     let randomNum = WINDOW_VALUES[Math.floor(Math.random() * WINDOW_VALUES.length)]
-      slotWindowEls.innerHTML = randomNum;
-      return randomNum
-  }
+for (let i = 0; i < slotWindowEls.length; i++){
+         let randomNum = WINDOW_VALUES[Math.floor(Math.random() * WINDOW_VALUES.length)]
+         slotWindowEls[i].innerText = randomNum  
+     } 
+     function winLoss() {
+         let s1 = slotWindowEls[0];
+         let s2 = slotWindowEls[1];
+         let s3 = slotWindowEls[2];
+         if (s1 === s2 && s1 === s3) {
+            console.log('you win')
+         }
+         
+         
 
-  function wagerAmount() {
-//bet needs to allow to player to bet 5$ or 10$ from their available balance
-    let newWager = 
   }
+winLoss();
+}
+  function betAmount() {
+//bet needs to allow to player to bet 5$ or 10$ from their available balance
+    
+
+//    let s2 = randomNum[1]
+//    let s3 = randomNum[2]
+
+//   console.log()
+//   if (s1 === s2 && s1 === s3) {
+//     return 
+    
+
+//   }
+ }
+
 
   function cashingOut() {
 //cashingOut need to essentially take the players current balance and cash it out
@@ -52,4 +77,6 @@ document.querySelector('#spinButton').addEventListener('click', spin);
 
   function render() {
     spin();
+    
+    
   }
