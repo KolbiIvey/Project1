2dc265204d22d14b1daf6734079862e70308c41a
KolbiIvey <kolbiivey@gmail.com>
2023-03-08 21:12:22 -0700

    updating my win loss logic

--- game.js
+++ game.js
@@ -2,23 +2,17 @@
 const WINDOW_VALUES = [2, 8, 5,]
 
   /*----- state variables -----*/
-let scores;
-
 let balance = 100;
-
-// let bet;
-
 
   /*----- cached elements  -----*/
 const slotWindowEls = Array.from(document.querySelectorAll('.slotMachine'));
 const betBtnEl = document.querySelector('#betButton')
-// const scoresEl = document.querySelector('#scores')
 const balanceEl = document.querySelector('#balance')
 const wonEl = document.querySelector('#win')
 const lostEl = document.querySelector('#Lost')
+
   /*----- event listeners -----*/
-document.querySelector('#spinButton').addEventListener('click', spin);
- document.querySelector('#betButton').addEventListener('click', betAmount)
+ document.querySelector('#betButton').addEventListener('click', spin)
 // document.querySelector('#cashOutButton').addEventListener('click', cashingOut)
 
   /*----- functions -----*/
@@ -26,11 +20,8 @@ document.querySelector('#spinButton').addEventListener('click', spin);
 
 
    function init() {
-      scores = { 
-            win: 0,
-            lost: 0
-      };
      balance = 100
+     balanceEl.innerText = `Current Balance: $${balance}`
      render();
   }
 
@@ -42,45 +33,40 @@ for (let i = 0; i < slotWindowEls.length; i++){
          slotWindowEls[i].innerText = randomNum  
      } 
      winLoss();
-
+     youLose();
+     wonEl.style.visibilty = 'hidden';
+     lostEl.style.visibilty = 'hidden';
 }
-  function betAmount() {
-//betAmount should take 10$ from players current balance before they can spin
-// if player wins player gets original 10$ back, plus an extra 10$
-//if player loses they lose the 10$ all together
-balanceEl.innerText = `Current Balance: ${balance}`
-let newBet = balance - 10;
-     console.log(newBet)
-  }
-      
-      
- function winLoss() {
-          let s1 = slotWindowEls[0].innerText;
-          let s2 = slotWindowEls[1].innerText;
-          let s3 = slotWindowEls[2].innerText;
-          if (s1 === s2 && s1 === s3) {
-             wonEl.style.visibility = 'visible';
-            
-            //  wonEl.innerText = `You won ${}`
-             return;
-          }else {
-            lostEl.style.visibility = 'visible';
-            // lostEl.innerText = `You lost ${}`
-          }
-          
-          
- 
-   }
+
+function winLoss() {
+    let s1 = slotWindowEls[0].innerText;
+    let s2 = slotWindowEls[1].innerText;
+    let s3 = slotWindowEls[2].innerText;
+    if (s1 === s2 && s1 === s3) {
+        balance += 15;
+             wonEl.innerText = `YOU WIN! Congrats your new balance is $${balance}`;
+             wonEl.style.visibilty = 'visible';
+    } else {
+        balance -= 10
+    }
+    balanceEl.innerText = `Current Balance: $${balance}`
+}
 
 
+function cashingOut() {
+}
 
-  function cashingOut() {
+function youLose() {
+    if (balance <= 0) {
+        lostEl.style.visibilty = 'visible';
+    } else {
+        return;
+    }
+}
 //cashingOut need to essentially take the players current balance and cash it out
 //(end the game and reset)
-  }
 
   function render() {
-    spin();
     
     
   }
